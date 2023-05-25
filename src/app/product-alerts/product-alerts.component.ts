afbafca29b2d4dd39695987d50a9bc97cfc17ec2
void-ness <78547746+void-ness@users.noreply.github.com>
2023-05-25 21:48:20 +0530

    init commit

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,12 @@
+import { Input, Component, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
